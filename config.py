2f77ad0f41261643cfac927d22c9118ad5d8cfde
0xc0def <lobo_prix@naver.com>
2021-02-20 13:17:11 +0900

    refactoring: spaces to tab, remove extra hard env

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class GameOption(object):
-    HUNGRY_RATE=20
-    ROW=7
-    COL=11
+	HUNGRY_RATE=20
+	ROW=7
+	COL=11
--- core.py
+++ core.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 from IPython import display
 
 def render_inline(env):
-    plt.figure(3)
-    plt.clf()
-    plt.imshow(env.render(mode='rgb_array'))
-    plt.axis('off')
+	plt.figure(3)
+	plt.clf()
+	plt.imshow(env.render(mode='rgb_array'))
+	plt.axis('off')
 
-    display.clear_output(wait=True)
-    display.display(plt.gcf())
+	display.clear_output(wait=True)
+	display.display(plt.gcf())
